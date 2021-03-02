591ec54e2b9d28ae15b72bd920a00ed98d5ea1a2
Alexey <cancem343@gmail.com>
2021-03-03 00:08:09 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
+exports.min = function min(array) {
+  if (array == null || array.length == 0) {
+      return 0;
+  }else return Math.min(...array);
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+  if (array == null || array.length == 0) {
+      return 0;
+  } else return Math.max(...array);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+  if (array == null || array.length == 0) {
+      return 0; 
+  }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+  let arrSum = 0;
+  array.forEach((elem) => {
+      arrSum += elem;
+  }); 
+  return arrSum / array.length;
+};
